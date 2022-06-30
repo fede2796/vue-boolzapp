0e20f925eacbb6ffa32c9a9554dc9a2bcac80cfc
fede2796 <fede27396@gmail.com>
2022-06-30 15:16:16 +0200

    Fine Milestone 5

---
+++ boolzapp-assets/Milestone5/js/script.js
@@ -0,0 +1,167 @@
+var  app = new Vue(
+	{
+		el: '#root',
+		data: {
+				userActiveChat: 2,
+				userText: '',
+                userfilter: '',
+				selectname: null,
+				contacts: [
+					{
+						name: 'Michele',
+						avatar: '_1',
+						visible: true,
+						messages: [
+							{
+								date: '10/01/2020 15:30:55',
+								text: 'Hai portato a spasso il cane?',
+								status: 'sent'
+							},
+							{
+								date: '10/01/2020 15:50:00',
+								text: 'Ricordati di dargli da mangiare',
+								status: 'sent'
+							},
+								{
+								date: '10/01/2020 16:15:22',
+								text: 'Tutto fatto!',
+								status: 'received'
+							}
+						],
+					},
+					{
+							name: 'Fabio',
+							avatar: '_2',
+							visible: true,
+							messages: [
+							{
+								date: '20/03/2020 16:30:00',
+								text: 'Ciao come stai?',
+								status: 'sent'
+							},
+							{
+								date: '20/03/2020 16:30:55',
+								text: 'Bene grazie! Stasera ci vediamo?',
+								status: 'received'
+							},
+							{
+								date: '20/03/2020 16:35:00',
+								text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+								status: 'sent'
+							}
+						],
+					},
+					{
+						name: 'Samuele',
+						avatar: '_3',
+						visible: true,
+						messages: [
+							{
+								date: '28/03/2020 10:10:40',
+								text: 'La Marianna va in campagna',
+									status: 'received'
+							},
+							{
+								date: '28/03/2020 10:20:10',
+								text: 'Sicuro di non aver sbagliato chat?',
+								status: 'sent'
+							},
+							{
+								date: '28/03/2020 16:15:22',
+								text: 'Ah scusa!',
+								status: 'received'
+							}
+						],
+					},
+					{
+						name: 'Luisa',
+						avatar: '_4',
+						visible: true,
+						messages: [
+							{
+								date: '10/01/2020 15:30:55',
+								text: 'Lo sai che ha aperto una nuova pizzeria?',
+								status: 'sent'
+							},
+							{
+								date: '10/01/2020 15:50:00',
+								text: 'Si, ma preferirei andare al cinema',
+								status: 'received'
+							}
+						],
+					},
+				]				
+			},
+			methods: {
+				//Cambio utente al click
+				chatUser(index){
+					
+					this.userActiveChat = index;
+				},
+				//Invio il messaggio
+				sendMessage(){
+					
+					const trimmedString = this.userText.trim();
+					
+					if(trimmedString.length > 0){
+						this.contacts[this.userActiveChat].messages.push({
+								date: dayjs().format("DD/MM/YYYY") + ' ' + dayjs().format("HH:mm:ss"),
+								text: trimmedString,
+								status: 'sent'
+							}
+						)
+						setTimeout(this.responseMessage,1000);							
+					}							
+					
+					this.userText = "";	
+				},
+				//Ricevo la risposta al messaggio
+				responseMessage(){
+					
+					this.contacts[this.userActiveChat].messages.push({
+						date: dayjs().format("DD/MM/YYYY") + ' ' + dayjs().format("HH:mm:ss"),
+						text: "ok",
+						status: 'received'
+					})
+				},
+				// Filtro i nomi all'interno della lista dei contatti
+                filterelements() {
+					
+					const userinput = this.userfilter.toLowerCase();
+					this.contacts.forEach((element) => {
+						const elementext = element.name.toLowerCase();
+
+						if(elementext.includes(userinput)) {
+							element.visible = true;
+						} else {
+							element.visible = false;
+						}
+					});
+				},
+				//Menu a tendina
+				curtainMenu(index){
+				if(this.selectname !== index){
+						this.selectname = index;
+					}
+					else{
+						this.selectname = null;
+					}
+				},
+				//Elimino il messagio nel mennu a tendina
+				messageDelete(indexmessage){
+					this.selectname = null;
+					this.contacts[this.userActiveChat].messages.splice(indexmessage, 1);
+				},
+				//Limito il testo del messaggio con slice
+				slicemessage(text){
+					let ntext = text;
+					if(text.length > 29){
+						ntext = text.slice(0,30);
+						ntext += "...";
+					}
+					return ntext;
+				}
+			
+			}
+	}
+);
